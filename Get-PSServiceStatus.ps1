127df1da4ee87b7c928612f49e1a1d84eb0f98b5
Dan Franciscus <dfranciscus@gmail.com>
2017-07-17 11:11:15 -1000

    Commit initial first

--- Get-PSServiceStatus.ps1
+++ Get-PSServiceStatus.ps1
@@ -2,21 +2,88 @@ function Get-PSServiceStatus {
 
 param (
     [string[]]$ComputerName,
-    [string[]]$ServiceName
+    [string]$ServiceName,
+    [string]$Path,
+    [string]$FromAddress,
+    [string]$ToAddress,
+    [string]$SmtpServer
 )
+    # Test ping
+    workflow Test-Ping 
+    {
+        param( 
+            [Parameter(Mandatory=$true)] 
+            [string[]]$Computers
+        )
+            foreach -parallel -throttlelimit 150 ($Computer in $Computers) 
+            {
+                if (Test-Connection -Count 1 $Computer -Quiet -ErrorAction SilentlyContinue) 
+                {    
+                    $Computer
+                }
+                else
+                {
+                    Write-Warning -Message "$Computer not online"
+                }
+            }
+        }
+    $ComputerName = Test-Ping -Computers $ComputerName 
 
     foreach ($Computer in $ComputerName)
     {
-    #Ping computer
+    $NewPath = Join-Path -Path $Path -ChildPath $Computer
+        #Get previous status
+        if (Test-Path -Path $NewPath)
+        {
+            $PreviousStatus = 'Not Running'
+        }
+        else 
+        {
+            $PreviousStatus = 'Running'    
+        }
 
-    #Get previous status
+        #Get current status
+        $CurrentStatus = Get-Service -Name $ServiceName -ComputerName $Computer | Where-Object {$_.Status -eq 'Running'}
+        if ($CurrentStatus)
+        {
+            $CurrentStatus = 'Running'
+        }
+        else 
+        {
+            $CurrentStatus = 'Not Running'    
+        }
+        
+        #Current status running and previous up
+        if ($PreviousStatus -eq 'Running' -and $CurrentStatus -eq 'Running')
+        {
+            Write-Warning -Message "$Computer $ServiceName still running"
+            Continue
+        }
 
-    #Get current status
+        #Current status running and previous down
+        if ($PreviousStatus -eq 'Not Running' -and $CurrentStatus -eq 'Running')
+        {
+            Write-Warning -Message "$Computer $ServiceName now running"
+            Remove-Item -Path $NewPath -Force | Out-Null
+            Send-MailMessage -Body ' ' -From "$FromAddress" -SmtpServer "$SmtpServer" -Subject "$Computer $ServiceName is now running" -To $ToAddress 
+            Continue
+        }
 
-    #Compare current and previous status
+        #Current status down and previous down 
+        if ($PreviousStatus -eq 'Not Running' -and $CurrentStatus -eq 'Not Running')
+        {
+            Write-Warning -Message "$Computer $ServiceName still not running"
+            New-Item -Path $Path$ComputerName -ItemType File -Force | Out-Null
+            Continue
+        }
 
-    #If change in status send mail
-
-    #Write current status to file
+        #Current status down and previous up 
+        if ($PreviousStatus -eq 'Running' -and $CurrentStatus -eq 'Not Running')
+        {
+            Write-Warning -Message "$Computer $ServiceName is not running"
+            New-Item -Path $NewPath -ItemType File -Force | Out-Null
+            Send-MailMessage -Body ' ' -From "$FromAddress" -SmtpServer "$SmtpServer" -Subject "$Computer $ServiceName is not running" -To $ToAddress 
+            Continue
+        }
     }
 }
